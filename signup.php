ab05e530d26307b007da38283d17906079cac97f
Chua Wei Wen <weiwen.chua5@gmail.com>
2017-09-26 16:13:07 +0800

    Add stylesheet

--- signup.php
+++ signup.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <head>
 <title>Sign up form</title>
+<link rel = "stylesheet"
+   type = "text/css"
+   href = "styles/style.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style>
 li {listt-style: none;}
@@ -21,15 +24,6 @@ li {listt-style: none;}
 </html>
 <?php
 $db = pg_connect("host=188.166.229.13 port=5455 dbname=crowdfunding user=postgres password=210217huhu");
-<<<<<<< HEAD
-$query = "SELECT add_user ('$_POST[email]','$_POST[name]', crypt('$_POST[pass]', gen_salt('bf', 8)))";
-$result = pg_query($query);
-
-$row = $pg_fetch_assoc($result);
-echo $row;
-
-echo "hello";
-=======
 if(isset($_POST['submit'])) {
 	$query = "SELECT add_user('$_POST[email]','$_POST[name]', crypt('$_POST[pass]', gen_salt('bf', 8)))";
 	//$query = "INSERT INTO account VALUES ('$_POST[email]','$_POST[name]', crypt('$_POST[pass]', gen_salt('bf', 8)))";
@@ -37,5 +31,4 @@ if(isset($_POST['submit'])) {
 	$row = pg_fetch_array($result);
 	echo $row[0];
 }
->>>>>>> 6ed3ce593642c76a303bc6e34dfd8d0a3b3d9409
 ?>
