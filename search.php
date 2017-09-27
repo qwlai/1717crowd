0d2353cfe91c513550d104f8e7ed3cc01c5dc0ec
qwlai <laiqiwei30@hotmail.com>
2017-09-27 09:52:01 +0800

    updated search

--- search.php
+++ search.php
@@ -9,37 +9,71 @@ $search_field = $_POST['search'];
 $result = pg_query_params($db, 'SELECT * FROM projectview WHERE title ilike $1', array("%".$search_field."%"));
 
 ?>
-
-<!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search</title>
+    <title>Untitled</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
 <body>
-<table width=100% border="1">
-<tr>
-<td><b>Advertiser</b></td> <td><b>title</b></td><td><b>description</b></td> <td><b>start_date</b></td> <td><b>end_date</b></td> <td><b>keywords</b></td> <td><b>amount sought</b></td>
-</tr>
-<?php while ($row = pg_fetch_array($result)) { ?> 
-<tr>
-	<td><?php echo $row['advertiser'] ?></td>
-	<td><?php echo $row['title'] ?></td>
-	<td><?php echo $row['description'] ?></td>
-	<td><?php echo $row['start_date'] ?></td>
-	<td><?php echo $row['end_date'] ?></td>
-	<td><?php echo $row['keywords'] ?></td>
-	<td><?php echo $row['amount_sought'] ?></td>
-</tr>	
-<?php } ?>
+    <div class="container">
+        <div class="table-responsive" style=>
+            <table class="table">
+                <thead>
+                    <tr>
+                    	<th style="display:none;">ProjectID</th>
+                        <th>Owner </th>
+                        <th>Title </th>
+                        <th>Description </th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Keywords</th>
+                        <th>Progress</th>
+                    </tr>
+                </thead>
+					<?php while ($row = pg_fetch_array($result)) { ?> 
+					<tr>
+						<td style="display:none;"><?php $project_id = $row['project_id']; echo $project_id; ?></td>
+						<td><?php echo $row['advertiser'] ?></td>
+						<td><?php echo $row['title'] ?></td>
+						<td><?php echo $row['description'] ?></td>
+						<td><?php echo $row['start_date'] ?></td>
+						<td><?php echo $row['end_date'] ?></td>
+						<td><?php echo $row['keywords'] ?></td>
 
-</table>
+						<td>
+							<div class="progress">
+									<?php  	$progress = pg_query_params($db, 'SELECT calculate_fund($1)', array($project_id));
+								   	$amount = pg_fetch_array($progress);
+								   	$amount_sought = $row['amount_sought'];
+								   	$percentage = round($amount[0]/$amount_sought*100,2);
+								   	
+								   	
+								   	if ($percentage > 100) { ?>
+  										<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">
+  										<?php echo $amount[0]."/".$amount_sought; ?>
+  										</div>
+								   	<?php } else { 
+								   		echo '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="'.$percentage.'" aria-valuemin="0" aria-valuemax="100" style="width:'.$percentage.'%">';
+  										echo $amount[0]."/".$amount_sought;
+  										echo '</div>';
+
+								   		} ?>
+  								<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+  								</div>
+							</div>	
+						</td>
+						<?php } ?>
+					</tr>	
+            </table>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
+
 </html>
