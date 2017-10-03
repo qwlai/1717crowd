b2aea6be6c31854eb522b3b7811690110b5419ac
qwlai <laiqiwei30@hotmail.com>
2017-10-03 14:02:46 +0800

    updated db

--- sql/crowdfunding.sql
+++ sql/crowdfunding.sql
@@ -1,27 +1,29 @@
 
 CREATE TABLE account (
-	email VARCHAR (255) PRIMARY KEY,
-	NAME VARCHAR (255) NOT NULL,
+	email VARCHAR (64) PRIMARY KEY,
+	NAME VARCHAR (64) NOT NULL,
 	password VARCHAR (255) NOT NULL,
 	is_admin BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE project (
-	project_id serial PRIMARY KEY,
-	advertiser VARCHAR (255) REFERENCES account (email),
-	title VARCHAR (255) NOT NULL,
+	advertiser VARCHAR (64) REFERENCES account (email),
+	title VARCHAR (64) NOT NULL,
 	description TEXT NOT NULL,
 	start_date DATE NOT NULL,
 	end_date DATE check(end_date > start_date) NOT NULL,
-	keywords VARCHAR (255),
+	keywords VARCHAR (64),
 	amount_sought NUMERIC check(amount_sought > 0) NOT NULL
+	primary key (advertiser, title)
 );
 
 CREATE TABLE fund (
-	fund_id serial PRIMARY KEY,
-	investor VARCHAR (255) REFERENCES account (email),
-	project_id serial REFERENCES project (project_id),
+	owner varchar(64),
+	title varchar(64), 
+	investor VARCHAR (64) REFERENCES account (email),
 	amount NUMERIC check(amount > 0) NOT NULL
+	primary key (owner, title, investor),
+	foreign key (owner, title) REFERENCES project(advertiser, title)
 );
 
 
